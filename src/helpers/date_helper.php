b18c80b2b57189e05d432eba4ad3c354511624c3
tomkirsch <mrmotts@gmail.com>
2024-10-17 17:33:39 -0400

    since Time no longer extends DateTime, allow Time instances to be converted

--- src/helpers/date_helper.php
+++ src/helpers/date_helper.php
@@ -1,16 +1,27 @@
 <?php
+
 use CodeIgniter\I18n\Time;
 
 // apply timezone in config to date
-function localDate($dateTime, string $format=NULL, string $timezone=NULL){
-	if($dateTime === NULL) return $format ? '' : NULL;
-	if(is_string($dateTime)) $dateTime = new Time($dateTime);
-	$dateTime = $dateTime->setTimezone($timezone ?? config('App')->localTimezone ?? config('App')->appTimezone);
+function localDate($dateTime, string $format = NULL, string $timezone = NULL)
+{
+	$app = config('App');
+	if (empty($app->localTimezone)) throw new Exception('localDate() expects a localTimezone property in config/App.php');
+	if (empty($app->appTimezone)) throw new Exception('localDate() expects an appTimezone property in config/App.php');
+	if ($dateTime === NULL) return $format ? '' : NULL;
+	if (is_string($dateTime)) $dateTime = new Time($dateTime, $app->appTimezone);
+	if (!is_a($dateTime, 'CodeIgniter\I18n\Time')) throw new Exception('localDate() expects a string or Time object');
+	$dateTime = $dateTime->setTimezone($timezone ?? $app->localTimezone);
 	return empty($format) ? $dateTime : $dateTime->format($format);
 }
-function serverDate($dateTime, string $format=NULL, string $timezone=NULL){
-	if($dateTime === NULL) return $format ? '' : NULL;
-	if(is_string($dateTime)) $dateTime = new Time($dateTime);
-	$dateTime = $dateTime->setTimezone($timezone ?? config('App')->appTimezone);
+function serverDate($dateTime, string $format = NULL, string $timezone = NULL)
+{
+	$app = config('App');
+	if (empty($app->localTimezone)) throw new Exception('localDate() expects a localTimezone property in config/App.php');
+	if (empty($app->appTimezone)) throw new Exception('serverDate() expects an appTimezone property in config/App.php');
+	if ($dateTime === NULL) return $format ? '' : NULL;
+	if (is_string($dateTime)) $dateTime = new Time($dateTime, $app->localTimezone);
+	if (!is_a($dateTime, 'CodeIgniter\I18n\Time')) throw new Exception('serverDate() expects a string or Time object');
+	$dateTime = $dateTime->setTimezone($timezone ?? $app->appTimezone);
 	return empty($format) ? $dateTime : $dateTime->format($format);
 }
--- src/helpers/form_helper.php
+++ src/helpers/form_helper.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 // properly transform variables into strings (such as DateTime) for comparison to POST
 // requires date_helper
-function make_string($val, array $options=[]):string{
-	if(is_string($val)) return $val;
-	if(is_null($val)) return '';
-	if(is_bool($val)) return $val ? '1' : '0';
-	
-	if($val instanceof DateTime){
-		if(!function_exists('localDate')) helper('date');
-		if(isset($options['timezone'])){
+function make_string($val, array $options = []): string
+{
+	if (is_string($val)) return $val;
+	if (is_null($val)) return '';
+	if (is_bool($val)) return $val ? '1' : '0';
+	if (is_a($val, 'DateTime') || is_a($val, 'CodeIgniter\I18n\Time')) {
+		if (!function_exists('localDate')) helper('date');
+		if (isset($options['timezone'])) {
 			// a specific timezone was passed, use it
 			$val = localDate($val, NULL, $options['timezone']); // date_helper
-		}else if(!empty($options['localTimezone'])){
+		} else if (!empty($options['localTimezone'])) {
 			// use the default local timezone
 			$val = localDate($val); // date_helper
 		}
-		if(empty($options['format'])){
+		if (empty($options['format'])) {
 			// guess if there's a time involved
 			$options['format'] = ($val->toTimeString() === '00:00:00') ? 'Y-m-d' : 'Y-m-d H:i:s';
 		}
@@ -25,60 +25,67 @@ function make_string($val, array $options=[]):string{
 	return (string) $val;
 }
 // remove brackets from input name
-function input_to_property($field){
+function input_to_property($field)
+{
 	preg_match('/\[(\w+)\]/', $field, $matches);
 	return empty($matches) ? $field : $matches[1];
 }
 // determine if object has the given property, and if so, return it
-function get_obj_value($obj, $prop, $default=NULL){
+function get_obj_value($obj, $prop, $default = NULL)
+{
 	// this does the weird work of checking for NULLs, because isset() will return FALSE if the property is NULL.
 	// is $obj empty/null?
-	if(empty($obj)){
+	if (empty($obj)) {
 		return $default;
 	}
-	if(is_a($obj, '\CodeIgniter\Entity\Entity')){
+	if (is_a($obj, '\CodeIgniter\Entity\Entity')) {
 		// Entities use magic methods, which will return FALSE on property_exists()
 		// if we convert it to an array, then we can just use array_key_exists()
 		$obj = $obj->toArray();
 	}
-	if(is_array($obj)){
-		if(!array_key_exists($prop, $obj)) return $default;
+	if (is_array($obj)) {
+		if (!array_key_exists($prop, $obj)) return $default;
 		return $obj[$prop];
-	}else{
-		if(!property_exists($obj, $prop)) return $default;
+	} else {
+		if (!property_exists($obj, $prop)) return $default;
 		return $obj->{$prop};
 	}
 }
-function set_value_from(string $field, $obj, $default='', bool $escape=TRUE, array $options=[]):string{
+function set_value_from(string $field, $obj, $default = '', bool $escape = TRUE, array $options = []): string
+{
 	$property = input_to_property($field);
 	$objValue = get_obj_value($obj, $property, $default);
 	$val = make_string($objValue, $options);
 	return set_value($field, $val, $escape);
 }
-function set_checkbox_from(string $field, $obj, $val, bool $default=FALSE, array $options=[]){
+function set_checkbox_from(string $field, $obj, $val, bool $default = FALSE, array $options = [])
+{
 	$property = input_to_property($field);
 	$notSetString = '____NOTSET____';
 	$objValue = get_obj_value($obj, $property, $notSetString);
 	$checked = ($objValue !== $notSetString && $objValue == $val);
-	if($objValue === $notSetString) $objValue = NULL;
-	if($checked) $default = TRUE;
+	if ($objValue === $notSetString) $objValue = NULL;
+	if ($checked) $default = TRUE;
 	$val = make_string($objValue, $options);
 	return set_checkbox($field, $val, $default);
 }
-function set_select_from(string $field, $obj, $val, bool $default=FALSE, array $options=[]){
+function set_select_from(string $field, $obj, $val, bool $default = FALSE, array $options = [])
+{
 	$property = input_to_property($field);
 	$notSetString = '____NOTSET____';
 	$objValue = get_obj_value($obj, $property, $notSetString);
 	$checked = ($objValue !== $notSetString && $objValue == $val);
-	if($objValue === $notSetString) $objValue = NULL;
-	if($checked) $default = TRUE;
+	if ($objValue === $notSetString) $objValue = NULL;
+	if ($checked) $default = TRUE;
 	$val = make_string($objValue, $options);
 	return set_select($field, $val, $default);
 }
-function set_radio_from(string $field, $obj, $val, bool $default=FALSE, array $options=[]){
+function set_radio_from(string $field, $obj, $val, bool $default = FALSE, array $options = [])
+{
 	return set_checkbox_from($field, $obj, $val, $default, $options);
 }
-function set_textarea_from(string $field, $obj, $default='', bool $escape=TRUE, array $options=[]){
+function set_textarea_from(string $field, $obj, $default = '', bool $escape = TRUE, array $options = [])
+{
 	$val = set_value_from($field, $obj, $default, FALSE, $options);
 	return $escape ? htmlspecialchars($val) : $val;
 }
